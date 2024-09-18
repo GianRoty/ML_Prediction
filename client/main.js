479aa3feab2b456f580ade374a2794409604a868
GianRoty <123582204+GianRoty@users.noreply.github.com>
2024-09-18 17:52:29 +0200

    JavaScript on working

--- client/main.js
+++ client/main.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
 	$("button").click(function(){
-		console.log($("car").val() + " e")
+		var obj = {
+			car : $("#car").val(), 
+			model : $("#model").val(), 
+			volume: $("#volume").val(), 
+			weight: $("#weight").val(), 
+			co2: $("#co2").val()
+		};
+		alert(obj.car)
 	});
 });
